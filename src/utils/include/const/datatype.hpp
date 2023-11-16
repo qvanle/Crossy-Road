48055bed47f6513df59a8dfa3ef8257ea8017a99
qvanle <vanle199tk@gmail.com>
2023-11-16 19:46:54 +0700

    add datatype

---
+++ src/utils/include/const/datatype.hpp
@@ -0,0 +1,18 @@
+#ifndef CONSTANT_HPP
+#define CONSTANT_HPP
+
+#include <array>
+#include <vector>
+#include <string>
+
+using iPoint = std::array<int, 2>;
+using fPoint = std::array<float, 2>;
+
+using iRect = std::array<int, 4>;
+using fRect = std::array<float, 4>;
+
+using vi = std::vector<int>;
+using vf = std::vector<float>;
+
+
+#endif 
