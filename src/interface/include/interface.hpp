a444c785bf0d7429cbb8c9b819125ff0bb042458
qvanle <vanle199tk@gmail.com>
2023-12-14 14:06:06 +0700

    declare load object, chunk, event, collide, control

--- src/interface/include/interface.hpp
+++ src/interface/include/interface.hpp
@@ -11,6 +11,12 @@ class Interface : public Container
 private: 
     std::vector<Container*> containers;
 
+protected:
+    void loadObject(YAML::Node);
+    void loadCollide(YAML::Node);
+    void loadChunk(YAML::Node);
+    void loadControl(YAML::Node);
+    void loadEvent(YAML::Node);
 public: 
     Interface(Frame*, Rectangle);
     Interface(Interface*);
@@ -20,6 +26,7 @@ public:
     ~Interface();
 
     std::string linkContent(std::string path) override;
+    std::string linkContentAbsolute(std::string path) override;
 
     Action* react() override;
 
--- src/interface/src/constructor.cpp
+++ src/interface/src/constructor.cpp
@@ -1,5 +1,6 @@
 #include <interface.hpp>
 #include <const/path/atb.hpp>
+#include <file.hpp>
 
 Interface::Interface(Frame* frame, Rectangle rect) : Container(frame, rect)
 {
@@ -19,5 +20,57 @@ Interface::Interface(Interface* other, Frame* frame, Rectangle rect) : Container
 
 std::string Interface::linkContent(std::string path)
 {
-    return Container::linkContentAbsolute(PATB::INTERFACE_ + path);
+    return linkContentAbsolute(PATB::INTERFACE_ + path);
 }
+
+std::string Interface::linkContentAbsolute(std::string path)
+{
+    YAML::Node node = YAML_FILE::readFile(path);
+    if(!loadName(node)) return "";
+    
+    if(node["textures"]) 
+        loadSprites(node["textures"]);
+    
+    if(node["focus"]) 
+        loadFocus(node["focus"]);
+    else chooseImage(0, 0);
+
+    if(node["object"]) 
+        loadObject(node["object"]);
+
+    if(node["collide"])
+        loadCollide(node["collide"]);
+
+    if(node["chunk"])
+        loadChunk(node["chunk"]);
+
+    if(node["control"])
+        loadControl(node["control"]);
+
+    if(node["event"])
+        loadEvent(node["event"]);
+
+    return getName();
+}
+
+void Interface::loadObject(YAML::Node node)
+{
+}
+
+void Interface::loadCollide(YAML::Node node)
+{
+}
+
+void Interface::loadChunk(YAML::Node node)
+{
+}
+
+void Interface::loadControl(YAML::Node node)
+{
+}
+
+void Interface::loadEvent(YAML::Node node)
+{
+}
+
+
