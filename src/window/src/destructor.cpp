874959e0b1c6ed4c43eba4064791f3dce2a0a98c
qvanle <vanle199tk@gmail.com>
2023-12-14 14:06:35 +0700

    fix bug: destructor more than 1

--- src/window/src/destructor.cpp
+++ src/window/src/destructor.cpp
@@ -3,10 +3,6 @@
 Window::~Window()
 {
 
-    UI.~UI();
-    immediate_pool.~ActionPool();
-    duration_pool.~ActionPool();
-
     for (auto& thread : Wcontent.thread_pool)
         thread.join();
 
