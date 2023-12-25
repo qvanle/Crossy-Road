64473eda6b4ffad730601eae0b51e12b551cd542
qvanle <vanle199tk@gmail.com>
2023-12-25 12:57:00 +0700

    change delay

--- src/window/src/running.cpp
+++ src/window/src/running.cpp
@@ -27,7 +27,7 @@ void Window::run() {
         //sound_effect();
         //userActing();
         //immediateActing();
-        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+        std::this_thread::sleep_for(std::chrono::milliseconds(5));
     }
 }
 
