9c0765c06ccb26710a28804106e8ad1667687364
QuangDuongg <22125081@student.hcmus.edu.vn>
2023-12-27 19:53:17 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,23 @@
 
 int main()
 {
-    Window win("window.yaml");
-    win.run();
+    // Window win("window.yaml");
+    // win.run();
+    highScore hs("saving/highscore.txt");
+    std::cout << "Origin high score:" << std::endl;
+    std::vector<std::pair<int, std::string>> list = hs.getList();
+    for (auto i : list)
+    {
+        std::cout << i.first << " " << i.second << std::endl;
+    }
+    hs.add("Huy", 100);
+    hs.add("linh chi", 300);
+    std::cout << "New high score:" << std::endl;
+    std::vector<std::pair<int, std::string>> list2 = hs.getList();
+    for (auto i : list2)
+    {
+        std::cout << i.first << " " << i.second << std::endl;
+    }
+    // hs.save("saving/highscore.txt");
     return 0;
 }
