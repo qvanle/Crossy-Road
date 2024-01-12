247831bb53490d5a486d7cab70bc3b0ac6b974b6
DuongMQuang <148790502+DuongMQuang@users.noreply.github.com>
2024-01-12 10:17:22 +0700

    updt gitignore

---
+++ build/_deps/yamlcpp-build/util/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Laptop K1/Desktop/CS202/Crossy-Road/build/_deps/yamlcpp-src/util
+# Build directory: C:/Users/Laptop K1/Desktop/CS202/Crossy-Road/build/_deps/yamlcpp-build/util
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
